72ffe3fc84180ef9ecccc3bfb0116de8337993d5
Matheus Albert <matheusal2018@outlook.com>
2021-03-15 14:50:02 -0300

    Index associação de camadas

--- WebApplication1/WebApplication1/Context/EFContext.cs
+++ WebApplication1/WebApplication1/Context/EFContext.cs
@@ -9,8 +9,13 @@ namespace WebApplication1.Context
 {
     public class EFContext : System.Data.Entity.DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
